937547d0ef05e361414ced42199065b35fbbf689
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2023-04-24 20:29:15 +0900

    Add docs directory

---
+++ docs/precache-manifest.9dad33b5447f98dabc35f63b170e78f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0e16f4d988f1a9112d5",
+    "url": "/static/css/main.5a26bfbd.chunk.css"
+  },
+  {
+    "revision": "c0e16f4d988f1a9112d5",
+    "url": "/static/js/main.c0e16f4d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ff32b404e1cc70c62ca4cfda94949230",
+    "url": "/index.html"
+  }
+];
